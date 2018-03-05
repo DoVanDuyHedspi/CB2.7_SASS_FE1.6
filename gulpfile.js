9d833d40f004a958f73e573c42a02822dff0264f
duy <vanduy07c.r@gmail.com>
2018-03-05 17:44:05 +0700

    update scss

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ gulp.task('sass',function(){
 		}))
 });
 gulp.task('watch',['browserSync','sass'],function(){
-	gulp.watch('app/css/**/*.scss',['sass']);
+	gulp.watch('app/scss/**/*.scss',['sass']);
 	gulp.watch('app/*html',browserSync.reload);
 	gulp.watch('app/js/**/*.js',browserSync.reload);
 });
